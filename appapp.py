f82ff9ff313fc1d62f6e839c635aeb509a245b86
Adam <bielaa3@gmail.com>
2025-03-07 03:23:31 +0100

    update 0

---
+++ appapp.py
@@ -0,0 +1,294 @@
+import json  # 0.25.0 bedzie mam potrzebny do zapisania pliku jako bazy danych
+from pathlib import Path  # 025.1
+import streamlit as st
+from openai import OpenAI
+from dotenv import dotenv_values  # do czytania z plików .env
+
+# vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+# WPROWADZAM NOWY SŁOWNIK SŁOWNIKÓW CENNIKI MODELI
+
+model_pricings = {
+    "gpt-4o": {  # NAJBARDZIEJ WSZECHSTRONNY MODEL OFEROWANY OPENAI
+        "input_tokens": 5.00 / 1_000_000,  # per token ZERA MOŻNA ROZDZIELAĆ W PYTONIE _
+        "output_tokens": 15.00 / 1_000_000,  # per token
+    },
+    "gpt-4o-mini": {  # SZYBSZA ZNACZNIE TAŃSZA WERSJA MODELU OFEROWANY OPENAI
+        "input_tokens": 0.150 / 1_000_000,  # per token
+        "output_tokens": 0.600 / 1_000_000,  # per token
+    }
+}
+
+with st.sidebar:
+    st.subheader("Aktualna konwersacja")
+    MODEL = st.selectbox('Wybrany model', ['gpt-4o-mini', 'gpt-4o'])
+
+USD_TO_PLN = 3.98  # KURS AKTUALNY
+PRICING = model_pricings[MODEL]  # ŁADUJEMY CENNIK DO UŻYWANEGO MODELU 
+
+# hej biblioteko dotenv_values przeczytaj mi rartości z .env
+env = dotenv_values(".env")
+#openai_client = OpenAI(api_key=env["OPENAI_API_KEY"])#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+def get_openai_client():
+    return OpenAI(api_key=st.session_state["openai_api_key"])
+
+#
+# CHATBOT
+#
+# nowa lista wiadomości memeory chatbot przyjmuje na wejściu jakieś zapytanie 
+def chatbot_reply(user_prompt, memory):
+    messages = [
+        {
+            "role": "system",
+            "content": st.session_state["chatbot_personality"],
+        },
+    ]
+    for message in memory:
+        messages.append({"role": message["role"], "contmodel": MODEL, "content": message["content"]})
+
+    messages.append({"role": "user", "contmodel": MODEL, "content": user_prompt})
+
+    response = OpenAI(api_key=st.session_state["openai_api_key"]).chat.completions.create(#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        model=MODEL,
+        messages=messages
+    )
+    usage = {}
+
+    if response.usage:
+        usage = {
+            "completion_tokens": response.usage.completion_tokens,
+            "prompt_tokens": response.usage.prompt_tokens,
+            "total_tokens": response.usage.total_tokens,
+        }
+
+    return {
+        "role": "assistant",
+        "content": response.choices[0].message.content,
+        "usage": usage,
+    }
+
+#
+# CONVERSATION HISTORY AND DATABASE
+#
+DEFAULT_PERSONALITY = """
+Jesteś pomocnikiem, który odpowiada na wszystkie pytania użytkownika.
+Odpowiadaj na pytania w sposób zwięzły i zrozumiały.
+""".strip()
+
+# Update path to local storage
+DB_PATH = Path("C:/GPT/db")  # New path for database
+DB_CONVERSATIONS_PATH = DB_PATH / "conversations"
+
+def load_conversation_to_state(conversation):
+    st.session_state["id"] = conversation["id"]
+    st.session_state["name"] = conversation["name"]
+    st.session_state["messages"] = conversation["messages"]
+    st.session_state["chatbot_personality"] = conversation["chatbot_personality"]
+
+def load_current_conversation():
+    if not DB_PATH.exists():
+        DB_PATH.mkdir(parents=True)  # Create the main DB directory
+        DB_CONVERSATIONS_PATH.mkdir()  # Create the conversations directory
+        conversation_id = 1
+        conversation = {
+            "id": conversation_id,
+            "name": "Konwersacja 1",
+            "chatbot_personality": DEFAULT_PERSONALITY,
+            "messages": [],
+        }
+
+        with open(DB_CONVERSATIONS_PATH / f"{conversation_id}.json", "w") as f:
+            f.write(json.dumps(conversation))
+
+        with open(DB_PATH / "current.json", "w") as f:
+            f.write(json.dumps({
+                "current_conversation_id": conversation_id,
+            }))
+    else:
+        with open(DB_PATH / "current.json", "r") as f:
+            data = json.loads(f.read())
+            conversation_id = data["current_conversation_id"]
+
+        with open(DB_CONVERSATIONS_PATH / f"{conversation_id}.json", "r") as f:
+            conversation = json.loads(f.read())
+
+    load_conversation_to_state(conversation)
+
+def save_current_conversation_messages():
+    conversation_id = st.session_state["id"]
+    new_messages = st.session_state["messages"]
+    with open(DB_CONVERSATIONS_PATH / f"{conversation_id}.json", "r") as f:
+        conversation = json.loads(f.read())
+
+    with open(DB_CONVERSATIONS_PATH / f"{conversation_id}.json", "w") as f:
+        f.write(json.dumps({
+            **conversation,
+            "messages": new_messages,
+        }))
+
+def save_current_conversation_name():
+    conversation_id = st.session_state["id"]
+    new_conversation_name = st.session_state["new_conversation_name"]
+
+    with open(DB_CONVERSATIONS_PATH / f"{conversation_id}.json", "r") as f:
+        conversation = json.loads(f.read())
+
+    with open(DB_CONVERSATIONS_PATH / f"{conversation_id}.json", "w") as f:
+        f.write(json.dumps({
+            **conversation,
+            "name": new_conversation_name,
+        }))
+
+def save_current_conversation_personality():
+    conversation_id = st.session_state["id"]
+    new_chatbot_personality = st.session_state["new_chatbot_personality"]
+
+    with open(DB_CONVERSATIONS_PATH / f"{conversation_id}.json", "r") as f:
+        conversation = json.loads(f.read())
+
+    with open(DB_CONVERSATIONS_PATH / f"{conversation_id}.json", "w") as f:
+        f.write(json.dumps({
+            **conversation,
+            "chatbot_personality": new_chatbot_personality,
+        }))
+
+def create_new_conversation():
+    conversation_ids = [int(p.stem) for p in DB_CONVERSATIONS_PATH.glob("*.json")]
+    conversation_id = max(conversation_ids, default=0) + 1
+    personality = DEFAULT_PERSONALITY
+    if "chatbot_personality" in st.session_state and st.session_state["chatbot_personality"]:
+        personality = st.session_state["chatbot_personality"]
+
+    conversation = {
+        "id": conversation_id,
+        "name": f"Konwersacja {conversation_id}",
+        "chatbot_personality": personality,
+        "messages": [],
+    }
+
+    with open(DB_CONVERSATIONS_PATH / f"{conversation_id}.json", "w") as f:
+        f.write(json.dumps(conversation))
+
+    with open(DB_PATH / "current.json", "w") as f:
+        f.write(json.dumps({
+            "current_conversation_id": conversation_id,
+        }))
+
+    load_conversation_to_state(conversation)
+    st.rerun()
+
+def switch_conversation(conversation_id):
+    with open(DB_CONVERSATIONS_PATH / f"{conversation_id}.json", "r") as f:
+        conversation = json.loads(f.read())
+
+    with open(DB_PATH / "current.json", "w") as f:
+        f.write(json.dumps({
+            "current_conversation_id": conversation_id,
+        }))
+
+    load_conversation_to_state(conversation)
+    st.rerun()
+
+def list_conversations():
+    conversations = []
+    for p in DB_CONVERSATIONS_PATH.glob("*.json"):
+        with open(p, "r") as f:
+            conversation = json.loads(f.read())
+            conversations.append({
+                "id": conversation["id"],
+                "name": conversation["name"],
+            })
+
+    return conversations
+
+#
+# MAIN PROGRAM
+#
+load_current_conversation()  # ładowanie do session_state-a messages i role
+
+st.title(":classical_building: NaszGPT")  # TYTUŁ
+
+# OpenAI API key protection
+if not st.session_state.get("openai_api_key"):
+    if "OPENAI_API_KEY" in env:
+        st.session_state["openai_api_key"] = env["OPENAI_API_KEY"]
+
+    else:
+        st.info("Dodaj swój klucz API OpenAI aby móc korzystać z tej aplikacji")
+        st.session_state["openai_api_key"] = st.text_input("Klucz API", type="password")
+        if st.session_state["openai_api_key"]:
+            st.rerun()
+
+if not st.session_state.get("openai_api_key"):
+    st.stop()
+
+
+# Wyświetlenie starych wiadomości użytkownika
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["contmodel"])
+        st.markdown(message["content"])
+
+# Chat input
+prompt = st.chat_input("O co chcesz spytać?")
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    st.session_state["messages"].append({"role": "user", "contmodel": MODEL, "content": prompt})
+
+    # Wyświetlenie odpowiedzi AI
+    with st.chat_message("assistant"):
+        response = chatbot_reply(prompt, memory=st.session_state["messages"][-10:]) 
+        st.markdown(MODEL + ":  " + "\n" + response["content"])
+
+    st.session_state["messages"].append({"role": "assistant", "contmodel": MODEL, "content": response["content"], "usage": response["usage"]})
+    save_current_conversation_messages()
+
+# Sidebar
+with st.sidebar:
+    st.write("Aktualny Kurs USD_TO_PLN", USD_TO_PLN)
+
+    total_cost = 0
+    for message in st.session_state.get("messages") or []:
+        if "usage" in message:
+            total_cost += message["usage"]["prompt_tokens"] * PRICING["input_tokens"]
+            total_cost += message["usage"]["completion_tokens"] * PRICING["output_tokens"]
+
+    c0, c1 = st.columns(2)
+    with c0:
+        st.metric("Koszt rozmowy (USD)", f"${total_cost:.4f}")
+
+    with c1:
+        st.metric("Koszt rozmowy (PLN)", f"{total_cost * USD_TO_PLN:.4f}")
+
+    st.session_state["name"] = st.text_input(
+        "Nazwa konwersacji",
+        value=st.session_state["name"],
+        key="new_conversation_name",
+        on_change=save_current_conversation_name,
+    )
+
+    st.session_state["chatbot_personality"] = st.text_area(
+        "Osobowość chatbota",
+        max_chars=1000,
+        height=200,
+        value=st.session_state["chatbot_personality"],
+        key="new_chatbot_personality",
+        on_change=save_current_conversation_personality,
+    )
+
+    st.subheader("Konwersacje")
+    if st.button("Nowa konwersacja"):
+        create_new_conversation()
+
+    # Pokażemy tylko top 5 konwersacji
+    conversations = list_conversations()
+    sorted_conversations = sorted(conversations, key=lambda x: x["id"], reverse=True)
+    for conversation in sorted_conversations[:10]:
+        c0, c1 = st.columns([10, 3])
+        with c0:
+            st.write(conversation["name"])
+
+        with c1:
+            if st.button("załaduj", key=conversation["id"], disabled=conversation["id"] == st.session_state["id"]):
+                switch_conversation(conversation["id"])
